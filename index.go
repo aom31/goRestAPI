3b3f2f82d1d4e30cb96c15797763b7b950dd422e
aom31 <97218117+aom31@users.noreply.github.com>
2022-06-13 01:41:15 +0700

    final

--- index.go
+++ index.go
@@ -17,10 +17,50 @@ func main() {
 	}
 
 	r := gin.Default()
-	r.GET("/users", func(c *gin.Context) {
+	r.GET("/users/read", func(c *gin.Context) {
 		var users []model.User
 		db.Find(&users)
 		c.JSON(http.StatusOK, users)
 	})
+	r.GET("/user/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var user model.User
+		db.First(&user, id)
+		c.JSON(http.StatusOK, user)
+	})
+
+	r.POST("/user/create", func(c *gin.Context) {
+		var user model.User
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		result := db.Create(&user)
+		c.JSON(200, gin.H{"RowsAffected": result.RowsAffected})
+
+	})
+	r.DELETE("/user/delete/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var user model.User
+		db.First(&user, id)
+		db.Delete(&user)
+	})
+	r.PUT("/user/update", func(c *gin.Context) {
+		var user model.User
+		var updateUser model.User
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		db.First(&updateUser, user.ID)
+		updateUser.Username = user.Username
+		updateUser.Fullname = user.Fullname
+		updateUser.Password = user.Password
+		updateUser.Email = user.Email
+
+		db.Save(&updateUser)
+		c.JSON(200, updateUser)
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
